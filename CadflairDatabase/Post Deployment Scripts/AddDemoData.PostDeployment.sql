3158c1254721f24f70e81beba60772cacd715d71
Justin Gaukler <jpgaukler@gmail.com>
2022-10-25 22:22:40 -0400

    Added post deployment script and compare script.

---
+++ CadflairDatabase/Post Deployment Scripts/AddDemoData.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+if not exists (select * from dbo.Account where Account.CompanyName = 'Demo Account')
+	begin
+		insert into dbo.Account (CompanyName)
+		values('Demo Account');
+end
+
+
+
