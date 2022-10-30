9860677f20ee2427b31b5add843d61ae00bf8008
Justin Gaukler <jpgaukler@gmail.com>
2022-10-30 09:18:36 -0400

    Added new project CadflairBlazorServer

---
+++ CadflairBlazorServer/Data/ForgeObject.cs
@@ -0,0 +1,12 @@
+﻿namespace CadflairBlazorServer.Data
+{
+    public class ForgeObject
+    {
+
+        public string ObjectKey { get; set; }
+        public string BucketKey { get; set; }
+        public string EncodedUrn { get; set; }
+        public string ThumbnailString { get; set; }
+
+    }
+}
