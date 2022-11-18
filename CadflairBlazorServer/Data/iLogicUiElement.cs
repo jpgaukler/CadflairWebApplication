022cbf7ac699290f4a1f168c35b280814e14789d
Justin Gaukler <jpgaukler@gmail.com>
2022-11-17 22:17:54 -0500

    Working on recursively rendering iLogic Ui components on Blazor.

---
+++ CadflairBlazorServer/Data/iLogicUiElement.cs
@@ -0,0 +1,15 @@
+﻿namespace CadflairBlazorServer.Data
+{
+    public class iLogicUiElement
+    {
+        public string UiElementSpec { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Guid { get; set; } = string.Empty;
+        public string? ToolTip { get; set; }
+        public string? EnablingParameterName { get; set; }
+        public string? ParameterName { get; set; }
+        public string? EditControlType { get; set; }
+        public string? ReadOnly { get; set; }
+        public iLogicUiElement[]? Items { get; set; }
+    }
+}
