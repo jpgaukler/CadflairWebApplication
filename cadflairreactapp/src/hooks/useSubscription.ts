e782cd3fe996a5dafeb6b9028cbdd052b9a6a9a9
Justin Gaukler <jpgaukler@gmail.com>
2024-01-06 09:26:20 -0500

    Testing out SWR on Categories page.

--- cadflairreactapp/src/hooks/useSubscription.ts
+++ cadflairreactapp/src/hooks/useSubscription.ts
@@ -1,9 +1,10 @@
 import useSWR from 'swr';
+import Subscription from '../interfaces/Subscription.interface';
 
 const fetcher = (url:string) => fetch(url).then(res => res.json())
 
 export function useSubscription(companyName:string) {
-    const { data, error, isLoading } = useSWR(`/api/v1/subscriptions/${companyName}`, fetcher)
+    const { data, error, isLoading } = useSWR<Subscription>(`/api/v1/subscriptions/${companyName}`, fetcher)
 
     return {
         subscription: data,
--- cadflairreactapp/src/pages/Categories.tsx
+++ cadflairreactapp/src/pages/Categories.tsx
@@ -1,70 +1,27 @@
 import { Image, Text, Group, Grid, Paper, Stack, Title } from '@mantine/core';
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Category from '../interfaces/Category.interface';
 import ProductDefinition from '../interfaces/ProductDefinition.interface';
 import Subscription from '../interfaces/Subscription.interface';
+import useSWR from 'swr';
 
+
+
+const fetcher = (url: string) => fetch(url).then(res => {
+    console.log(res);
+    return res.json();
+})
 
 
 export default function Categories() {
     const params = useParams();
     const navigate = useNavigate();
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [productDefinitions, setProductDefinitions] = useState<ProductDefinition[]>([]);
-    const [subscription, setSubscription] = useState<Subscription>();
 
-    useEffect(() => {
+    const { data: subscription } = useSWR<Subscription>(`/api/v1/subscriptions/${params.companyName}`, fetcher)
+    const { data: categories, error, isLoading } = useSWR<Category[]>(subscription ? `/api/v1/subscriptions/${subscription.id}/categories/` : null, fetcher)
+    //const { data: categories, error, isLoading } = useSWR<Category[]>(subscription ? `/api/v1/subscriptions/${subscription.id}/categories/${params.categoryName ? params.categoryName : ""}` : null, fetcher)
+    //const { data:productDefinitions, error, isLoading } = useSWR<ProductDefinition[]>(`/api/v1/subscriptions/${subscription.id}/categories/${category.id}/product-definitions`, fetcher)
 
-        // get subscription from api
-        fetch(`/api/v1/subscriptions/${params.companyName}`)
-            .then((response) => response.json())
-            .then((subscription:Subscription) => {
-                //console.log(subscription);
-                setSubscription(subscription);
-
-                if (params.categoryName) {
-                    // get single category from api
-                    fetch(`/api/v1/subscriptions/${subscription.id}/categories/${params.categoryName}`)
-                        .then((response) => response.json())
-                        .then((category: Category) => {
-                            //console.log(category);
-                            setCategories(category.childCategories);
-
-                            // get product definitions for this category
-                            fetch(`/api/v1/subscriptions/${subscription.id}/categories/${category.id}/product-definitions`)
-                                .then((response) => response.json())
-                                .then((productDefinitions: ProductDefinition[]) => {
-                                    console.log(productDefinitions);
-                                    setProductDefinitions(productDefinitions);
-                                })
-                                .catch((err) => {
-                                    console.log(err.message);
-                                });
-                        })
-                        .catch((err) => {
-                            console.log(err.message);
-                        });
-
-                }
-                else {
-                    // get root categories from api
-                    fetch(`/api/v1/subscriptions/${subscription.id}/categories/`)
-                        .then((response) => response.json())
-                        .then((categories: Category[]) => {
-                            //console.log(categories);
-                            setCategories(categories);
-                            setProductDefinitions([]);
-                        })
-                        .catch((err) => {
-                            console.log(err.message);
-                        });
-                }
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }, [params.categoryName, params.companyName, params.subscriptionId]);
 
     return (
         <>
@@ -96,24 +53,24 @@ export default function Categories() {
                 <Grid.Col span={12}  >
                     <Title order={5}>Product Definitions</Title>
                 </Grid.Col>
-                {productDefinitions?.map(productDefinition =>
-                    <Grid.Col key={productDefinition.id} span={{ base: 12, md: 6, lg: 3 }}>
-                        <Paper shadow="xs" p="sm" onClick={() => handleProductDefinitionClick(productDefinition)}>
-                            <Group>
-                                <Image
-                                    src={productDefinition.thumbnailUri}
-                                    height={130}
-                                    width={75}
-                                    alt="Norway"
-                                />
-                                <Stack>
-                                    <Text fw={500}>{productDefinition.name}</Text>
-                                    <Text size="sm" c="dimmed">{productDefinition.description}</Text>
-                                </Stack>
-                            </Group>
-                        </Paper>
-                    </Grid.Col>
-                )}
+            {/*    {productDefinitions?.map(productDefinition =>*/}
+            {/*        <Grid.Col key={productDefinition.id} span={{ base: 12, md: 6, lg: 3 }}>*/}
+            {/*            <Paper shadow="xs" p="sm" onClick={() => handleProductDefinitionClick(productDefinition)}>*/}
+            {/*                <Group>*/}
+            {/*                    <Image*/}
+            {/*                        src={productDefinition.thumbnailUri}*/}
+            {/*                        height={130}*/}
+            {/*                        width={75}*/}
+            {/*                        alt="Norway"*/}
+            {/*                    />*/}
+            {/*                    <Stack>*/}
+            {/*                        <Text fw={500}>{productDefinition.name}</Text>*/}
+            {/*                        <Text size="sm" c="dimmed">{productDefinition.description}</Text>*/}
+            {/*                    </Stack>*/}
+            {/*                </Group>*/}
+            {/*            </Paper>*/}
+            {/*        </Grid.Col>*/}
+            {/*    )}*/}
             </Grid>
         </>
     );
